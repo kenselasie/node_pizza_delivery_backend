d6b668776c615477a352560bb62502709218ef88
kenselasie <kenselasie@gmail.com>
2021-12-12 02:48:38 +0000

    Update: Added Email helper function

--- src/controllers/helpers/helperFunctions.js
+++ src/controllers/helpers/helperFunctions.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"
+import mailgun from "mailgun-js"
 
 export const validMongoDBId = (id) => {
     if (mongoose.Types.ObjectId.isValid(id)) return true
@@ -6,3 +7,15 @@ export const validMongoDBId = (id) => {
 }
 
 
+export const sendEmailMessage = (from, to, subject='Pizza Receipt', text, callback ) => {
+    const DOMAIN = process.env.MAILGUN_DOMAIN;
+    const APIKEY = process.env.MAILGUN_APIKEY
+    const mg = mailgun({apiKey: APIKEY, domain: DOMAIN});
+    const data = {
+        from, to, subject, text
+    };
+    
+    mg.messages().send(data, function (error, body) {
+        callback(error, body)
+    });
+}
